127e3993e174a9a429f3534976d51d6ece2caa92
kitagawarisa <kitakawauso@gmail.com>
2022-07-27 21:16:35 +0900

    :shirt: css fixed

--- js/visualization.js
+++ js/visualization.js
@@ -1,6 +1,3 @@
-const canvasWidth = 1280;
-const canvasHeight = 720;
-
 const node = [
   [1, 4], //   0: nose
   [2], //   1: left eye inner
@@ -38,37 +35,41 @@ const node = [
 ];
 
 function setup() {
-  let canvas = createCanvas(canvasWidth, canvasHeight, WEBGL);
+  let canvas = createCanvas(windowWidth / 2, windowHeight / 2, WEBGL);
   canvas.parent("canvas");
   angleMode(DEGREES);
   normalMaterial();
   // debugMode(); // display grid
 
-  camera(100, -100, 350, 0, 0, 0, 0, 1, 0);
+  camera(300, -100, 330, 100, 50, 0, 0, 1, 0);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth / 2, windowHeight / 2);
 }
 
 function viewReset() {
-  camera(100, -100, 350, 0, 0, 0, 0, 1, 0);
+  camera(300, 0, 350, 0, 0, 0, 0, 1, 0);
 }
 
 function drawPlane() {
   push();
-  translate(0, -80, -160);
+  translate(0, 0, -160);
   noFill(0);
   stroke(200, 200, 0);
-  plane(320, 160); // xy plane
+  plane(320, 320); // xy plane
   pop();
 
   push();
-  translate(-160, -80, 0);
+  translate(-160, 0, 0);
   rotateY(90);
   noFill(0);
   stroke(0, 200, 200);
-  plane(320, 160); // yz plane
+  plane(320, 320); // yz plane
   pop();
 
   push();
-  translate(0, 0, 0);
+  translate(0, 160, 0);
   rotateX(90);
   noFill(0);
   stroke(200, 0, 200);
@@ -79,12 +80,12 @@ function drawPlane() {
 function drawStroke(data, color) {
   for (var i = 0; i < node.length; i++) {
     let x1 = data[i].x * 300 - 200;
-    let y1 = data[i].y * 300 - 250;
+    let y1 = data[i].y * 300 - 100;
     let z1 = -data[i].z * 200;
     for (var j = 0; j < node[i].length; j++) {
       let d = node[i][j];
       let x2 = data[d].x * 300 - 200;
-      let y2 = data[d].y * 300 - 250;
+      let y2 = data[d].y * 300 - 100;
       let z2 = -data[d].z * 200;
 
       stroke(color[0], color[1], color[2]);
@@ -97,7 +98,7 @@ function drawSphere(data, color) {
   for (var i = 0; i < 33; i++) {
     // console.log(data[i]);
     let x = data[i].x * 300 - 200;
-    let y = data[i].y * 300 - 250;
+    let y = data[i].y * 300 - 100;
     let z = -data[i].z * 200;
 
     push();
